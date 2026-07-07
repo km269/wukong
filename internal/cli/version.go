2f473162fd4ffadeed4dcfba9359d44025a14137
一片会思考的猪肉 <prolead@163.com>
2026-07-07 19:21:05 +0800

    0.2.3

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -8,9 +8,9 @@ import (
 
 // Version information set at build time via ldflags.
 var (
-	Version   = "0.2.2"
+	Version   = "0.2.3"
 	GitCommit = "unknown"
-	BuildDate = "2026-07-5"
+	BuildDate = "2026-07-7"
 )
 
 func newVersionCmd() *cobra.Command {
